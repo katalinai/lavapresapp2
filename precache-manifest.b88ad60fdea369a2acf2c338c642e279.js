27eb7fe5829940b94fc4a6b6869fe2a6d88b4e3f
KatalinaI <laura.ibave@gmail.com>
2019-09-16 23:22:54 -0500

    Updates

---
+++ precache-manifest.b88ad60fdea369a2acf2c338c642e279.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75a693c1c77dea5094fed03fa8f6a834",
+    "url": "/lavapresapp2/index.html"
+  },
+  {
+    "revision": "1170a986e5028765b8e7",
+    "url": "/lavapresapp2/static/css/main.f0850378.chunk.css"
+  },
+  {
+    "revision": "3e9be7b065a01a9f28f2",
+    "url": "/lavapresapp2/static/js/2.4049ad4f.chunk.js"
+  },
+  {
+    "revision": "1170a986e5028765b8e7",
+    "url": "/lavapresapp2/static/js/main.530af108.chunk.js"
+  },
+  {
+    "revision": "0260d97ecaee47335019",
+    "url": "/lavapresapp2/static/js/runtime~main.c2df91df.js"
+  },
+  {
+    "revision": "a5cce2cdca237e3df94429d42240b12d",
+    "url": "/lavapresapp2/static/media/calendario.a5cce2cd.svg"
+  },
+  {
+    "revision": "210ef1124440a85bb77a89ef6c8e52d5",
+    "url": "/lavapresapp2/static/media/icono-lavapres-blanco.210ef112.svg"
+  },
+  {
+    "revision": "f6cbe9eaac14926178b09213200943ea",
+    "url": "/lavapresapp2/static/media/logotipo-lavapres.f6cbe9ea.svg"
+  },
+  {
+    "revision": "3cf97837524dd7445e9d1462e3c4afe2",
+    "url": "/lavapresapp2/static/media/open-iconic.3cf97837.woff"
+  },
+  {
+    "revision": "47151e87c5a8666791a91007de4962da",
+    "url": "/lavapresapp2/static/media/open-iconic.47151e87.eot"
+  },
+  {
+    "revision": "93cc7aa654ae36b0828716f5ec3159cd",
+    "url": "/lavapresapp2/static/media/open-iconic.93cc7aa6.ttf"
+  },
+  {
+    "revision": "eca2c26759a009a4a9361151efe99e74",
+    "url": "/lavapresapp2/static/media/open-iconic.eca2c267.svg"
+  },
+  {
+    "revision": "6cf7172a7bfcf4e62f3dcd2c6c9af341",
+    "url": "/lavapresapp2/static/media/paquete01.6cf7172a.png"
+  },
+  {
+    "revision": "3b238901a793d3b4ed120b8b5459e118",
+    "url": "/lavapresapp2/static/media/paquete02.3b238901.png"
+  },
+  {
+    "revision": "3b238901a793d3b4ed120b8b5459e118",
+    "url": "/lavapresapp2/static/media/paquete03.3b238901.png"
+  },
+  {
+    "revision": "daea2f4b92a7f38fea6a77dd4cc1be2d",
+    "url": "/lavapresapp2/static/media/torreA.daea2f4b.png"
+  },
+  {
+    "revision": "043478329ef7ec2a3b70ed16e19e3242",
+    "url": "/lavapresapp2/static/media/torreB.04347832.png"
+  }
+]);
